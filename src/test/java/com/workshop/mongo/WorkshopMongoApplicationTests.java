bf4dff51fd7e7fc840ea11cefca897c40d6294a4
Lelton Borges <lelton.borges1@gmail.com>
2020-03-25 20:05:59 -0300

    project created

---
+++ src/test/java/com/workshop/mongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
