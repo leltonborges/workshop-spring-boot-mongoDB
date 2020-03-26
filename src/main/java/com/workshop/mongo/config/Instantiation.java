5711863221c686db0c967a8667d159d3125ce796
Lelton Borges <lelton.borges1@gmail.com>
2020-03-25 22:32:19 -0300

    Operation of instatiation to data base

---
+++ src/main/java/com/workshop/mongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.workshop.mongo.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.workshop.mongo.domain.User;
+import com.workshop.mongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
